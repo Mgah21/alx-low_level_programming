36f2c443998970c04d6c416ed5fd512a599883bc
MohammedGebril <mjbryl72@gmail.com>
2023-09-07 13:28:16 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ Return:always 0 (success)
 */ 
 int main (void )
 {
-puts (/" programming is like building a multilingual puzzle");
+puts (""/" programming is like building a multilingual puzzle");
 return (0);
 }
