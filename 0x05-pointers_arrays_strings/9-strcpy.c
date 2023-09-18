a44f5e7c73c7967e65c850f50bb12e39668a0fb3
MohammedGebril <mjbryl72@gmail.com>
2023-09-18 03:04:39 -0700

    task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = -1;
-
-do {
+while (src[i] != '\0')
+{
 i++;
-dest[i] = src[i]; }
-while (src[i] != '\0');
+dest[i] = src[i];
+}
 return (dest);
 }
