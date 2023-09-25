263028e703cffef4e35bdeda2511afcd7b0e43fa
MohammedGebril <mjbryl72@gmail.com>
2023-09-25 12:28:33 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strspn - Gets  length  a prefix substring.
+* @s: String where substring will look.
+* @accept: Substring of accepted characters.
+* Return: Length of occurrence.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int u = 0;
+char *t = accept;
+
+while (*s++)
+{
+while (*accept++)
+if (*(s - 1) == *(accept - 1))
+{
+u++;
+break;
+}
+if (!(*--accept
+break;
+accept = t;
+}
+return (u);
+}
