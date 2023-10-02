836e94e5ba0f20953f4aa1bf2b347613b5ebdc1f
MohammedGebril <mjbryl72@gmail.com>
2023-10-02 16:24:07 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function name start program with it
+ * @argc: offset counter
+ * @argv: offset values
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
