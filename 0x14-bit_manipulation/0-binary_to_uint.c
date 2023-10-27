2c21ce33a57dd83c7d85938ccdd928d5c91633a4
Mohammed Gebril <mjbryl72@gmail.com>
2023-10-26 18:27:48 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,14 +17,14 @@ unsigned int binary_to_uint(const char *b)
 
 	for (l = 0; b[l]; l++)
 
-	for (int i = l - 1; i >= 0; i--)
+while (l)
 {
-		if (b[i] != '0' && b[i] != '1')
+		if (b[l-1] != '0' && b[l-1] != '1')
 {
 			return (0);
 		}
 
-		if (b[i] == '1')
+		if (b[l] == '1')
 {
 			r += baces;
 		}
