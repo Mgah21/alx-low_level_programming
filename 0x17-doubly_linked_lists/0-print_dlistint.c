4e51019a18c0f22beafd4f6410183c6fcb2176d0
MohammedGebril <mjbryl72@gmail.com>
2023-12-07 20:53:24 +0200

    4

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t a;
+size_t a;
 
-  for (a = 0; h != NULL; a++)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
-  return (a);
+for (a = 0; h != NULL; a++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (a);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,21 +8,21 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-  int c;
+int c;
 
-  c = 0;
+c = 0;
 
-  if (h == NULL)
-    return (c);
+if (h == NULL)
+return (c);
 
-  while (h->prev != NULL)
-    h = h->prev;
+while (h->prev != NULL)
+h = h->prev;
 
-  while (h != NULL)
-    {
-      c++;
-      h = h->next;
-    }
+while (h != NULL)
+{
+c++;
+h = h->next;
+}
 
-  return (c);
+return (c);
 }
