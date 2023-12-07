f6a90621f6551d3bb2aa1118380f57e06d7ac82e
MohammedGebril <mjbryl72@gmail.com>
2023-12-07 18:07:21 +0200

    1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,9 +8,21 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t a;
+  int c;
 
-  for (a = 0; h != NULL; a++)
-    h = h->next;
-  return (a);
+  c = 0;
+
+  if (h == NULL)
+    return (c);
+
+  while (h->prev != NULL)
+    h = h->prev;
+
+  while (h != NULL)
+    {
+      c++;
+      h = h->next;
+    }
+
+  return (c);
 }
